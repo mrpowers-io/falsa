0a2cb7bb8154c74a07ce358a0c255f07506109d9
semyonsinchenko <ssinchenko@apache.org>
2025-01-26 16:19:36 +0100

    Fix?

--- tests/tests_main.py
+++ tests/tests_main.py
@@ -1,8 +1,7 @@
+import hashlib
 import os
 import sys
-import hashlib
-from subprocess import Popen, PIPE, STDOUT
-
+from subprocess import PIPE, STDOUT, Popen
 
 SHA256SUM_CSV_SMALL = "bab41ac749ed0d884a89b9b87869c7ce1d41ce8b8eb48819be06da8c274b5a04"
 CSV_FILE_NAME = "G1_1e7_1e7_100_0.csv"
@@ -16,12 +15,15 @@ if __name__ == "__main__":
         sys.stdout.flush()
         sys.exit(1)
 
-    hash = ""
+    hash = hashlib.sha256()
     with open(CSV_FILE_NAME, "rb", buffering=0) as file_:
-        hash = hashlib.file_digest(file_, "sha256").hexdigest()
+        file_bytes = file_.readall()
+        hash.update(file_bytes)
 
-    if not SHA256SUM_CSV_SMALL == hash:
-        sys.stdout.write(f"Wrong hash. Expected {SHA256SUM_CSV_SMALL} but got {hash}")
+    hash_str = hash.hexdigest()
+
+    if not SHA256SUM_CSV_SMALL == hash_str:
+        sys.stdout.write(f"Wrong hash. Expected {SHA256SUM_CSV_SMALL} but got {hash_str}")
         sys.stdout.flush()
         sys.exit(1)
 
