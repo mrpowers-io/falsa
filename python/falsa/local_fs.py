0f7fab93b6e045b5c0e5fdf1a8705a22fe1d447a
semyonsinchenko <ssinchenko@apache.org>
2025-02-12 18:09:52 +0100

    Rewrite joins + refactoring

--- python/falsa/local_fs.py
+++ python/falsa/local_fs.py
@@ -1,18 +1,20 @@
 from __future__ import annotations
 
-
+from abc import ABC, abstractmethod
 import random
 from typing import Iterator
 
+import numpy as np
 import pyarrow as pa
 
 from falsa import H2ODatasetSizes
 
 from .native import (
     generate_groupby,
-    generate_join_dataset_big,
-    generate_join_dataset_medium,
-    generate_join_dataset_small,
+    generate_join_lhs,
+    generate_join_rhs_big,
+    generate_join_rhs_medium,
+    generate_join_rhs_small,
 )
 
 NATIVE_I64_MAX_VALUE = 9_223_372_036_854_775_806
@@ -63,95 +65,7 @@ class GroupByGenerator:
             yield generate_groupby(self.n, self.k, self.nas, batch["seed"], batch["size"])
 
 
-class JoinSmallGenerator:
-    """A simple wrapper on top of native generator.
-
-    The class takes care of random seeds generation, input validation
-    and calculation of the size of all batches.
-    """
-
-    def __init__(
-        self,
-        size: H2ODatasetSizes | int,
-        n_rows: int,
-        k: int,
-        seed: int = 42,
-        keys_seed: int = 142,
-        batch_size: int = 5_000_000,
-    ) -> None:
-        _validate_int64(size, "size")
-        if (k < 0) or (k > size):
-            raise ValueError(f"k should be positive and less than {size} but got {k}")
-        if (batch_size <= 0) or (batch_size > size):
-            raise ValueError(f"batch size should be positive and less than {size} but got {batch_size}")
-        self.n: int = size
-        self.n_rows = n_rows
-        self.k = k
-        self.keys_seed = keys_seed
-
-        num_batches = self.n_rows // batch_size
-        batches = [batch_size for _ in range(num_batches)]
-        # A corner case when we need to add one more batch
-        if self.n_rows % batch_size != 0:
-            batches.append(self.n_rows % batch_size)
-
-        # Generate a random seed per batch
-        random.seed(seed)
-        self.batches = [{"size": bs, "seed": random.randint(0, NATIVE_I64_MAX_VALUE)} for bs in batches]
-
-    def iter_batches(self) -> Iterator[pa.RecordBatch]:
-        for batch in self.batches:
-            yield generate_join_dataset_small(self.n, batch["seed"], self.keys_seed, batch["size"])
-
-
-class JoinMediumGenerator:
-    """A simple wrapper on top of native generator.
-
-    The class takes care of random seeds generation, input validation
-    and calculation of the size of all batches.
-    """
-
-    def __init__(
-        self,
-        size: H2ODatasetSizes | int,
-        n_rows: int,
-        k: int,
-        seed: int = 42,
-        keys_seed: int = 142,
-        batch_size: int = 5_000_000,
-    ) -> None:
-        _validate_int64(size, "size")
-        if (k < 0) or (k > size):
-            raise ValueError(f"k should be positive and less than {size} but got {k}")
-        if (batch_size <= 0) or (batch_size > size):
-            raise ValueError(f"batch size should be positive and less than {size} but got {batch_size}")
-        self.n: int = size
-        self.n_rows = n_rows
-        self.k = k
-        self.keys_seed = keys_seed
-
-        num_batches = self.n_rows // batch_size
-        batches = [batch_size for _ in range(num_batches)]
-        # a corner case when we need to add one more batch
-        if self.n_rows % batch_size != 0:
-            batches.append(self.n_rows % batch_size)
-
-        # Generate a random seed per batch
-        random.seed(seed)
-        self.batches = [{"size": bs, "seed": random.randint(0, NATIVE_I64_MAX_VALUE)} for bs in batches]
-
-    def iter_batches(self) -> Iterator[pa.RecordBatch]:
-        for batch in self.batches:
-            yield generate_join_dataset_medium(self.n, batch["seed"], self.keys_seed, batch["size"])
-
-
-class JoinBigGenerator:
-    """A simple wrapper on top of native generator.
-
-    The class takes care of random seeds generation, input validation
-    and calculation of the size of all batches.
-    """
-
+class JoinGenerator(ABC):
     def __init__(
         self,
         size: H2ODatasetSizes | int,
@@ -169,20 +83,145 @@ class JoinBigGenerator:
             raise ValueError(f"batch size should be positive and less than {size} but got {batch_size}")
         self.n: int = size
         self.n_rows = n_rows
-        self.nas = nas
         self.k = k
         self.keys_seed = keys_seed
+        self.nas = nas
 
-        num_batches = self.n // batch_size
+        self.kk1 = self.generate_keys(int(self.n / 1e6))
+        self.kk2 = self.generate_keys(int(self.n / 1e3))
+        self.kk3 = self.generate_keys(int(self.n))
+
+        num_batches = self.n_rows // batch_size
         batches = [batch_size for _ in range(num_batches)]
         # A corner case when we need to add one more batch
-        if self.n % batch_size != 0:
-            batches.append(self.n % batch_size)
+        if self.n_rows % batch_size != 0:
+            batches.append(self.n_rows % batch_size)
 
         # Generate a random seed per batch
         random.seed(seed)
         self.batches = [{"size": bs, "seed": random.randint(0, NATIVE_I64_MAX_VALUE)} for bs in batches]
+        self.get_keys()
+
+    def generate_keys(self, nn: int) -> dict[str, np.ndarray]:
+        total_size = int(nn * 1.1)
+        npr = np.random.default_rng(seed=self.keys_seed)
+        key = npr.permutation(total_size) + 1
+
+        x_end = int(nn * 0.9)
+        l_end = int(nn * 1.1)
+
+        return {"x": key[:x_end], "l": key[x_end:nn], "r": key[nn:l_end]}
+
+    def sample_all(self, size: int, arr: np.ndarray, seed: int) -> np.ndarray:
+        assert len(arr) <= size, "Input length should be less than size!"
+
+        npr = np.random.default_rng(seed=seed)
+        if size > len(arr):
+            extra = npr.choice(arr, size - len(arr), replace=True)
+            result = np.concatenate([arr, extra])
+        else:
+            result = arr
+
+        return result
+
+    @abstractmethod
+    def get_keys(self) -> None: ...
+
+
+class JoinSmallGenerator(JoinGenerator):
+    """A simple wrapper on top of native generator.
+
+    The class takes care of random seeds generation, input validation
+    and calculation of the size of all batches.
+    """
+
+    def get_keys(self) -> None:
+        self.k1 = self.sample_all(int(self.n / 1e6), np.concatenate([self.kk1["x"], self.kk1["r"]]), self.keys_seed + 1)
 
     def iter_batches(self) -> Iterator[pa.RecordBatch]:
+        prev_batch = 0
         for batch in self.batches:
-            yield generate_join_dataset_big(self.n, self.nas, batch["seed"], self.keys_seed, batch["size"])
+            _t = prev_batch
+            prev_batch += batch["size"]
+            yield generate_join_rhs_small(
+                self.n, batch["seed"], pa.array(self.k1[_t:prev_batch], type=pa.int64()), batch["size"]
+            )
+
+
+class JoinMediumGenerator(JoinGenerator):
+    """A simple wrapper on top of native generator.
+
+    The class takes care of random seeds generation, input validation
+    and calculation of the size of all batches.
+    """
+
+    def get_keys(self) -> None:
+        self.k1 = self.sample_all(int(self.n / 1e3), np.concatenate([self.kk1["x"], self.kk1["r"]]), self.keys_seed + 1)
+        self.k2 = self.sample_all(int(self.n / 1e3), np.concatenate([self.kk2["x"], self.kk2["r"]]), self.keys_seed + 2)
+
+    def iter_batches(self) -> Iterator[pa.RecordBatch]:
+        prev_batch = 0
+        for batch in self.batches:
+            _t = prev_batch
+            prev_batch += batch["size"]
+            yield generate_join_rhs_medium(
+                self.n,
+                batch["seed"],
+                pa.array(self.k1[_t:prev_batch], type=pa.int64()),
+                pa.array(self.k2[_t:prev_batch], type=pa.int64()),
+                batch["size"],
+            )
+
+
+class JoinBigGenerator(JoinGenerator):
+    """A simple wrapper on top of native generator.
+
+    The class takes care of random seeds generation, input validation
+    and calculation of the size of all batches.
+    """
+
+    def get_keys(self) -> None:
+        self.k1 = self.sample_all(self.n, np.concatenate([self.kk1["x"], self.kk1["r"]]), self.keys_seed + 1)
+        self.k2 = self.sample_all(self.n, np.concatenate([self.kk2["x"], self.kk2["r"]]), self.keys_seed + 2)
+        self.k3 = self.sample_all(self.n, np.concatenate([self.kk3["x"], self.kk3["r"]]), self.keys_seed + 3)
+
+    def iter_batches(self) -> Iterator[pa.RecordBatch]:
+        prev_batch = 0
+        for batch in self.batches:
+            _t = prev_batch
+            prev_batch += batch["size"]
+            yield generate_join_rhs_big(
+                self.n,
+                batch["seed"],
+                pa.array(self.k1[_t:prev_batch], type=pa.int64()),
+                pa.array(self.k2[_t:prev_batch], type=pa.int64()),
+                pa.array(self.k3[_t:prev_batch], type=pa.int64()),
+                batch["size"],
+            )
+
+
+class JoinLHSGenerator(JoinGenerator):
+    """A simple wrapper on top of native generator.
+
+    The class takes care of random seeds generation, input validation
+    and calculation of the size of all batches.
+    """
+
+    def get_keys(self) -> None:
+        self.k1 = self.sample_all(self.n, np.concatenate([self.kk1["x"], self.kk1["l"]]), self.keys_seed + 1)
+        self.k2 = self.sample_all(self.n, np.concatenate([self.kk2["x"], self.kk2["l"]]), self.keys_seed + 2)
+        self.k3 = self.sample_all(self.n, np.concatenate([self.kk3["x"], self.kk3["l"]]), self.keys_seed + 3)
+
+    def iter_batches(self) -> Iterator[pa.RecordBatch]:
+        prev_batch = 0
+        for batch in self.batches:
+            _t = prev_batch
+            prev_batch += batch["size"]
+            yield generate_join_lhs(
+                self.n,
+                batch["seed"],
+                pa.array(self.k1[_t:prev_batch], type=pa.int64()),
+                pa.array(self.k2[_t:prev_batch], type=pa.int64()),
+                pa.array(self.k3[_t:prev_batch], type=pa.int64()),
+                batch["size"],
+            )
--- python/falsa/utils.py
+++ python/falsa/utils.py
@@ -1,16 +1,185 @@
 from __future__ import annotations
 
 import json
+import shutil
 import time
+from enum import Enum
 from pathlib import Path
 from uuid import uuid4
 
-from pyarrow import Schema
+import pyarrow as pa
+from pyarrow import Schema, csv, parquet
+from rich import print
+
+from falsa import H2ODatasetSizes
 
 PA_2_DELTA_DTYPES = {
     "int32": "integer",
     "int64": "long",
 }
+
+
+class Size(str, Enum):
+    SMALL = "SMALL"
+    MEDIUM = "MEDIUM"
+    BIG = "BIG"
+
+    def _to(self) -> H2ODatasetSizes:
+        # Workaround. Typer does not support IntEnum
+        if self is Size.SMALL:
+            return H2ODatasetSizes.SMALL
+        elif self is Size.MEDIUM:
+            return H2ODatasetSizes.MEDIUM
+        else:
+            return H2ODatasetSizes.BIG
+
+
+class Format(str, Enum):
+    CSV = "CSV"
+    DELTA = "DELTA"
+    PARQUET = "PARQUET"
+
+    def pprint(self):
+        print(f"An output format is [green]{self.value}[/green]")
+        print("\nBatch mode is supported.")
+        print("In case of memory problems you can try to reduce a [green]batch_size[/green].")
+        print()
+
+
+# An amount of rows per dataset is n // divisor
+DIVISORS = {
+    "groupby": 1,
+    "join_big": 1,
+    "join_big_na": 1,
+    "join_small": 1_000_000,
+    "join_medium": 1_000,
+    "join_lhs": 1,
+}
+
+
+class Schemas(Enum):
+    GROUPBY = pa.schema(
+        [
+            ("id1", pa.utf8()),
+            ("id2", pa.utf8()),
+            ("id3", pa.utf8()),
+            ("id4", pa.int64()),
+            ("id5", pa.int64()),
+            ("id6", pa.int64()),
+            ("v1", pa.int64(), False),
+            ("v2", pa.int64(), False),
+            ("v3", pa.float64(), False),
+        ]
+    )
+    JOIN_LHS = pa.schema(
+        [
+            ("id1", pa.int64(), False),
+            ("id2", pa.int64(), False),
+            ("id3", pa.int64(), False),
+            ("id4", pa.utf8()),
+            ("id5", pa.utf8()),
+            ("id6", pa.utf8()),
+            ("v1", pa.float64()),
+        ]
+    )
+    JOIN_RHS_SMALL = pa.schema(
+        [
+            ("id1", pa.int64(), False),
+            ("id4", pa.utf8()),
+            ("v2", pa.float64()),
+        ]
+    )
+    JOIN_RHS_MEDIUM = pa.schema(
+        [
+            ("id1", pa.int64(), False),
+            ("id2", pa.int64(), False),
+            ("id4", pa.utf8()),
+            ("id5", pa.utf8()),
+            ("v2", pa.float64()),
+        ]
+    )
+    JOIN_RHS_BIG = pa.schema(
+        [
+            ("id1", pa.int64(), False),
+            ("id2", pa.int64(), False),
+            ("id3", pa.int64(), False),
+            ("id4", pa.utf8()),
+            ("id5", pa.utf8()),
+            ("id6", pa.utf8()),
+            ("v2", pa.float64()),
+        ]
+    )
+
+
+def create_filename(ds_type: str, n: int, k: int, nas: int, fmt: Format) -> str:
+    if fmt is Format.DELTA:
+        suffix = ""
+    else:
+        suffix = "." + fmt.lower()
+    output_names = {
+        "groupby": "G1_{n}_{n}_{k}_{nas}{fmt}",
+        "join_big": "J1_{n}_{n}_NA{fmt}",
+        "join_big_na": "J1_{n}_{n}_{nas}{fmt}",
+        "join_small": "J1_{n}_{n_divided}_{nas}{fmt}",
+        "join_medium": "J1_{n}_{n_divided}_{nas}{fmt}",
+        "join_lhs": "J1_{n}_NA_{nas}{fmt}",
+    }
+
+    n_divisor = DIVISORS[ds_type]
+    n_divided = n // n_divisor
+    return output_names[ds_type].format(n=pretty_sci(n), n_divided=pretty_sci(n_divided), k=k, nas=nas, fmt=suffix)
+
+
+def clear_prev_if_exists(fp: Path, fmt: Format) -> None:
+    if fp.exists():
+        # All is file, delta is directory
+        if fmt is not Format.DELTA:
+            fp.unlink()
+        else:
+            shutil.rmtree(fp, ignore_errors=True)
+
+
+def get_writer(data_format: Format, schema: Schema, output_filepath: Path) -> csv.CSVWriter | parquet.ParquetWriter:
+    data_format.pprint()
+    print()
+
+    if data_format is Format.CSV:
+        return csv.CSVWriter(sink=output_filepath, schema=schema)
+    elif data_format is Format.PARQUET:
+        return parquet.ParquetWriter(where=output_filepath, schema=schema)
+    else:
+        delta_file_pq = output_filepath.joinpath("data.parquet")
+        return parquet.ParquetWriter(where=delta_file_pq, schema=schema)
+
+
+def pretty_sci(n: int) -> str:
+    # See https://github.com/duckdblabs/db-benchmark/blob/main/_data/groupby-datagen.R#L5
+    # pretty_sci = function(x) {
+    #     tmp<-strsplit(as.character(x), "+", fixed=TRUE)[[1L]]
+    #     if(length(tmp)==1L) {
+    #             paste0(substr(tmp, 1L, 1L), "e", nchar(tmp)-1L)
+    #     } else if(length(tmp)==2L){
+    #         paste0(tmp[1L], as.character(as.integer(tmp[2L])))
+    #     }
+    # }
+    if n == 0:
+        return "NA"
+
+    # format in scientific notation and remove +
+    formatted_num = f"{n:.0e}".replace("+", "")
+    e_value = int(formatted_num.split("e")[1])
+
+    if e_value >= 10:
+        return formatted_num
+
+    elif e_value == 0:
+        return formatted_num.replace("00", "0")
+
+    elif e_value < 10:
+        return formatted_num.replace("0", "")
+
+    else:
+        raise ValueError("Unexpected value following e")
 
 
 def generate_delta_log(output_filepath: Path, schema: Schema) -> None:
